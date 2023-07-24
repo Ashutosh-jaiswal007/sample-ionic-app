0d59e73e21a504f6ad118e6217be9a10a662e79d
Ashutosh Jaiswal <ashutosh.jaiswal@daffodilsw.com>
2023-07-24 15:48:39 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,32 @@
+import { InaiCheckout } from 'capacitor-inai';
+
 import logo from './logo.svg';
 import './App.css';
+import { useEffect } from 'react';
 
 function App() {
+
+  useEffect(()=>{
+    const payload = {
+      action: 'getInstalledApps',
+      paymentMethodOption: 'upi',
+    };
+    const config={
+      token:"testToken",
+      orderId:'test123',
+      countryCode:'test'
+    }
+    InaiCheckout.execute({ ...config, payload }).then((response) => {
+      console.log('wwwwwwww', response);
+      // success handler
+    }).catch((error) => {
+      // error handler
+    });
+  })
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       
       </header>
     </div>
   );
